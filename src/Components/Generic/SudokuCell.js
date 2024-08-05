807ec2af11eef1c2e26a250b728ce385a462fb51
HemyDay <sire.amanda@gmail.com>
2024-08-05 13:48:31 +0200

    update

--- src/Components/Generic/SudokuCell.js
+++ src/Components/Generic/SudokuCell.js
@@ -5,7 +5,6 @@ import React from "react";
 // styles ------------------------------------------------------------------
 import './SudokuCell.css';
 
-
 // --- COMPONENT --- //
 function SudokuCell(props) {
   return (
--- src/Components/Generic/SudokuGrid.js
+++ src/Components/Generic/SudokuGrid.js
@@ -19,6 +19,7 @@ function SudokuGrid() {
       ))}
     </div>
   );
+  
 }
 
 // --- EXPORT --- //
