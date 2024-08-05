f8130ab8fb6e99558e3e543c080aa7f561b7a92d
HemyDay <sire.amanda@gmail.com>
2024-08-05 11:29:57 +0200

    First Commit

---
+++ src/Components/Page/Page_GridEdit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page_GridEdit() {
+  return (
+    <div >
+      this is the page where you edit grids
+    </div>
+  );
+}
+
+
+export default Page_GridEdit;
