33deb5f34a2c0009f3e14af39fb3168b745c80c4
HemyDay <sire.amanda@gmail.com>
2024-08-21 11:36:09 +0200

    fixed number imput bug

--- src/Functions/handleErrors.js
+++ src/Functions/handleErrors.js
@@ -1,42 +1,35 @@
 
-const duplicateFinder = (cell, gridObject) => {
-  let output = cell.isWarning;
-    for (let i = 0; i < gridObject.length; i++) {
-      let ccell = gridObject[i];
-      if (
-        ( 
-          ccell.id[0] === cell.id[0] || 
-          ccell.id[1] === cell.id[1] ||
-          ccell.id[2] === cell.id[2] ||
-          ccell.zone.substring(0,2) === cell.zone.substring(0,2)
-        )
-        &&
-        ( 
-          ccell.id !== cell.id &&
-          ccell.value === cell.value 
-        ) 
-        &&
-        (
-          ccell.value !== 0 && cell.value !== 0
-        )
-      )
-      {
-        if (output.includes(ccell.id) === false){
-          output.push(ccell.id);
-        }
-      } else {
-        if (output.indexOf(ccell.id) !== -1){
-          output.splice(output.indexOf(ccell.id), 1);
-        }
-      }
-    }
-  return output;
-};
-
-
 const checkDuplicate = (cell, gridObject) => {
   let cellOutput = cell;
-  cellOutput.isWarning = duplicateFinder(cell, gridObject);  
+  for (let i = 0; i < gridObject.length; i++) {
+    let ccell = gridObject[i];
+    if (
+      ( 
+        ccell.id[0] === cell.id[0] || 
+        ccell.id[1] === cell.id[1] ||
+        ccell.id[2] === cell.id[2] ||
+        ccell.zone.substring(0,2) === cell.zone.substring(0,2)
+      )
+      &&
+      ( 
+        ccell.id !== cell.id &&
+        ccell.value === cell.value 
+      ) 
+      &&
+      (
+        ccell.value !== 0 && cell.value !== 0
+      )
+    )
+    {
+      if (cellOutput.isWarning.includes(ccell.id) === false){
+        cellOutput.isWarning.push(ccell.id);
+      }
+    } else {
+      if (cellOutput.isWarning.indexOf(ccell.id) !== -1){
+        cellOutput.isWarning.splice(cellOutput.isWarning.indexOf(ccell.id), 1);
+      }
+    }
+  }
   return cellOutput;
 }
 
--- src/Functions/handleNumberPress.js
+++ src/Functions/handleNumberPress.js
@@ -7,7 +7,7 @@ const handleNumberChange = (cell, number, isInNoteMode, setGridObject, gridObjec
       if (newCell.value >= 0 && newCell.value <= 9) {newCell.value = [0,0,0,0,0,0,0,0,0]}
       if (newCell.value[number-1] === 0 ) {newCell.value[number-1] = number} else {newCell.value[number-1] = 0}
     } else {
-      newCell.value === number ? newCell.value = 0 : newCell.value = Number(number);
+      newCell.value === Number(number) ? newCell.value = 0 : newCell.value = Number(number);
     }
     return newCell;                                                                     
   } else {
