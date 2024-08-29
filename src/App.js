84a08efbd020af99e372c8d9ae5e530bba64ca68
HemyDay <sire.amanda@gmail.com>
2024-08-29 13:24:23 +0200

    removed shift and capslock for note mode

--- src/App.js
+++ src/App.js
@@ -17,13 +17,14 @@ function App() {
   const POSSIBLE_KEY_INPUTS = [];
   const [PRESSED_KEY, setPRESSED_KEY] = useState(null)
   const [GRID_OBJECT, setGRID_OBJECT] = useState(Grid.grid_with_zones)
+  const [NOTE_MODE, setNOTE_MODE] = useState(false);
 
   // FUNCTIONS
   const onKeyRelease = () => {setPRESSED_KEY(null);};                     // Key release handler function, sets presedKey to null
-  const onKeyPress = (keyPressed, shift, capslock) => {                                    // Key press handler function
+  const onKeyPress = (keyPressed) => {                                    // Key press handler function
     setPRESSED_KEY(keyPressed);
     switch (true) {
-      case keyPressed >= 0 && keyPressed <= 9:  handleNumberPress(keyPressed, capslock, setGRID_OBJECT, GRID_OBJECT); break;
+      case keyPressed >= 0 && keyPressed <= 9:  handleNumberPress(keyPressed, setGRID_OBJECT, GRID_OBJECT, NOTE_MODE); break;
       default: break;
     }
 };
--- src/Functions/handleNumberPress.js
+++ src/Functions/handleNumberPress.js
@@ -1,9 +1,9 @@
 import handleErrors from "./handleErrors.js";
 import removeNotesOnPlacement from "./removeNotesOnPlacement.js";
 
-const handleNumberChange = (cell, number, GRID_OBJECT, capslock) => {
+const handleNumberChange = (cell, number, GRID_OBJECT, NOTE_MODE) => {
   if (cell.isSelected && cell.isEditable) {                                                                                                    
-    if (capslock === true) {
+    if (NOTE_MODE === true) {
       if (cell.value >= 0 && cell.value <= 9) {cell.value = [0,0,0,0,0,0,0,0,0]}
       if (cell.value[number-1] === 0 ) {cell.value[number-1] = number} else {cell.value[number-1] = 0}
     } 
@@ -14,8 +14,8 @@ const handleNumberChange = (cell, number, GRID_OBJECT, capslock) => {
   }
 }
 
-const handleNumberPress = (number, capslock, setGRID_OBJECT, GRID_OBJECT) => {
-  setGRID_OBJECT(GRID_OBJECT.map((cell) => handleNumberChange(cell, number, GRID_OBJECT)));
+const handleNumberPress = (number, setGRID_OBJECT, GRID_OBJECT, NOTE_MODE) => {
+  setGRID_OBJECT(GRID_OBJECT.map((cell) => handleNumberChange(cell, number, GRID_OBJECT, NOTE_MODE)));
   handleErrors(GRID_OBJECT, setGRID_OBJECT);
 }
 
